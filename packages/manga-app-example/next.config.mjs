dda24e3d9abcae32abe16651570a6108a74ca613
Terry Zhang <terrychinaz@gmail.com>
2024-09-17 01:28:30 +0800

    feat: ignoreBuildErrors

--- packages/manga-app-example/next.config.mjs
+++ packages/manga-app-example/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true, // Disable TypeScript build errors
+    },
+};
 
 export default nextConfig;
