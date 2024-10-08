01b33da808f2abd691c6bc9192ef2cde78b8c4f8
Terry Zhang <terrychinaz@gmail.com>
2024-10-09 00:00:56 +0800

    feat: add examples

--- packages/nextjs-app-examples/next.config.mjs
+++ packages/nextjs-app-examples/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production'?"https://webtoon-nextjs-app-examples.vercel.app/":'',
+  basePath:"/examples",
   env: {
     siteName: "LlamaGen.Ai",
     siteTitle:
